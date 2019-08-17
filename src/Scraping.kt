7a157b70b7429aef02b3ef35e27e189a714ab812
Mitsuhiro0911 <outoforder3216@gmail.com>
2019-08-17 15:45:40 +0900

    抽出する要素を指定する処理を追加

--- src/Scraping.kt
+++ src/Scraping.kt
@@ -1,6 +1,17 @@
 import org.jsoup.Jsoup
+import java.awt.SystemColor.text
+
+
+
+
 
 fun main () {
+    // URLの指定
     val document = Jsoup.connect("https://ja.wikipedia.org/wiki/メインページ").get()
     println(document.html())
+    // 要素の指定
+    val elements = document.select("h2")
+    for (element in elements) {
+        System.out.println(element.text())
+    }
 }
